1e7aa675e9ec0ae6eb3d71c18d5d870c881e621e
talhaaydin <mt.aydin@outlook.com>
2025-02-05 23:59:21 +0300

    font and color theme

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const App = () => {
   return (
     <main>
-      <h1 className="text-5xl text-orange-500 font-bold">Welcome to GSAP</h1>
+      <h1 className="text-5xl text-violet-300 font-bold">Welcome to GSAP</h1>
     </main>
   );
 };

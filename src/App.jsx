f29c0a875a2a1ac737e6b02163640bedf5568498
talhaaydin <mt.aydin@outlook.com>
2025-02-06 01:15:25 +0300

    Hero Section and animations with GSAP

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import Hero from "./components/Hero";
 
 const App = () => {
   return (
-    <main>
-      <h1 className="text-5xl text-violet-300 font-bold">Welcome to GSAP</h1>
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      {/*     <h1 className="text-5xl text-violet-300 font-bold">Welcome to GSAP</h1> */}
+      <Hero></Hero>
+      <section className="z-0 min-h-screen bg-blue-500"></section>
     </main>
   );
 };

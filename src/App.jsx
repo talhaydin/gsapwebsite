9d2b4288c0a8b5302dc5339b354f19f65aac1983
talhaaydin <mt.aydin@outlook.com>
2025-02-08 18:33:04 +0300

    navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import Hero from "./components/Hero";
 import About from "./components/About";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
-      {/*     <h1 className="text-5xl text-violet-300 font-bold">Welcome to GSAP</h1> */}
+      <Navbar></Navbar>
       <Hero></Hero>
-      {/*       <section className="z-0 min-h-screen bg-blue-500"></section> */}
       <About></About>
     </main>
   );
